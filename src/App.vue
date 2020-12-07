429679fdfb5de3380269ee4304c0f11927e5ea0f
wchrth <watcharathorn.l@gmail.com>
2020-12-08 00:59:32 +0700

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar variant="light">
+      <b-navbar-brand to="/">
+        <b-img v-bind="logoProps"/> Shiba Book Shop
+      </b-navbar-brand>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      logoProps: {
+        src: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+        class: 'd-inline-block align-top',
+        alt: 'Shiba Book Shop',
+        width: 30,
+        height: 30
+      }
     }
   }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="8"></b-col>
+      <b-col cols="4"></b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
