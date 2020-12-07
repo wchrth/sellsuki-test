3f9dac5af91584319cf578b847f0e3f6e8140959
wchrth <watcharathorn.l@gmail.com>
2020-12-08 02:32:16 +0700

    add book list component

---
+++ src/components/BookList.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-card>
+    <b-row class="align-items-stretch">
+      <b-col v-for="book in bookList" :key="book.id" class="mb-3" sm="6" lg="4">
+        <b-card
+          class="h-100"
+          :img-src="book.cover"
+          :img-alt="book.title"
+          img-top
+          :title="book.title"
+          :sub-title="`${book.price} Baht`"
+        />
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'BookList',
+  data () {
+    return {
+      bookList: []
+    }
+  },
+  created () {
+    this.fetchBookList()
+  },
+  methods: {
+    fetchBookList () {
+      axios.get('https://api.jsonbin.io/b/5f3154b06f8e4e3faf2f99de').then(({ data }) => {
+        this.bookList = data.books
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-img-top {
+  max-height: 200px;
+  min-height: 150px;
+  object-fit: contain;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <b-container fluid>
+  <b-container fluid class="py-3">
     <b-row>
-      <b-col cols="8"></b-col>
-      <b-col cols="4"></b-col>
+      <b-col md="8">
+        <BookList/>
+      </b-col>
+      <b-col md="4"></b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import BookList from '@/components/BookList.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    BookList
+  }
 }
 </script>
